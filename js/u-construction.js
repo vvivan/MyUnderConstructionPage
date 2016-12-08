89f787883281edd87dc9cc7be0e083a150ad7e4f
vvivan <outlaw_serni@yahoo.com>
2016-12-08 21:37:11 +0100

    Favicon and Animation
    Favicon and Animation

--- js/u-construction.js
+++ js/u-construction.js
@@ -9,4 +9,5 @@ var q = new Array(); // declare q as an array
 q[0] = new qte ("Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.", "Martin Golding");
 q[1] = new qte ("If debugging is the process of removing software bugs, then programming must be the process of putting them in.", "Edsger Dijkstra");
 
-//document.getElementById ("q-text").innerHTML = q[1].text;
+/*document.getElementById("quote").innerHTML = q[1].text;
+document.getElementById("author").innerHTML = q[1].author;*/
