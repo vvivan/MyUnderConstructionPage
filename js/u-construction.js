6acd8c01b08a53d42e54131ee93280409d4cb9ee
vvivan <outlaw_serni@yahoo.com>
2016-12-08 07:12:08 +0100

    Layout and JavaSC
    Layout and JavaSC

---
+++ js/u-construction.js
@@ -0,0 +1,12 @@
+function qte (text, author) {
+	this.text = text;
+	this.author = author;
+};
+
+var q = new Array(); // declare q as an array
+
+//declare new objects
+q[0] = new qte ("Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.", "Martin Golding");
+q[1] = new qte ("If debugging is the process of removing software bugs, then programming must be the process of putting them in.", "Edsger Dijkstra");
+
+//document.getElementById ("q-text").innerHTML = q[1].text;
